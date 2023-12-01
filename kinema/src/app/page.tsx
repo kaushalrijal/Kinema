03545c00e15e813d65bd882296500bec9ef19399
kaushalrijal <kaushalrijal091@gmail.com>
2023-12-01 16:55:29 +0545

    navbar done for now

--- kinema/src/app/page.tsx
+++ kinema/src/app/page.tsx
@@ -1,9 +1,26 @@
 import Image from 'next/image'
+import logo from './img/logo.png'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+    <main className="">
+      <div className='flex w-screen h-16 bg-slate-100 flex-row justify-between px-3 items-center py-auto'>
+      <Image
+      src={logo}
+      alt="kinema logo"
+      className={"h-[40px] w-[120px]"}
+    />
+      <div className="flex justify-between text-black gap-4">
+        <span>Home</span>
+        <span>Movies</span>
+        <span>Shows</span>
+        <span>Misc</span>
+      </div>
+      </div>
+      <div>
+        <div>Some subtext goes here</div>
+        <div>Searchbar goes here</div>
+      </div>
     </main>
   )
 }
