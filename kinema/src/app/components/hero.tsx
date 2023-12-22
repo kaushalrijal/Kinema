77a0359eca04a875a4e802ca1c21724906618e99
kaushalrijal <kaushalrijal091@gmail.com>
2023-12-22 12:20:31 +0545

    hero section made dynamic with new popular movie suggestion everytime

--- kinema/src/app/components/hero.tsx
+++ kinema/src/app/components/hero.tsx
@@ -1,37 +1,38 @@
+import { getPopularMovies } from "@/utils/request";
 import bg from "../../../public/collage.jpg";
 
-export default function Hero() {
+export default async function Hero(props) {
+  const suggested = await getPopularMovies();
+  const random = suggested[Math.floor(Math.random() * 20)];
   return (
     <section
-      className={`relative  bg-cover bg-center bg-no-repeat w-full font-medium   h-full pb-0 items-center justify-center`}
-      style={{ backgroundImage: `url(${bg.src})` }}
+      className={`relative  bg-cover bg-center bg-no-repeat w-full font-medium h-max-screen h-full pb-0 items-center justify-center`}
+      style={{
+        backgroundImage: `url(https://www.themoviedb.org/t/p/original/${random.backdrop_path})`,
+      }}
     >
       <div className={`absolute inset-0 bg-white/80`}></div>
 
       <div className="relative mx-auto max-w-full-xl px-4 py-32 sm:px-6 lg:flex lg:h-fit lg:items-center lg:px-8 ">
         <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
           <h1 className="text-3xl font-extrabold sm:text-5xl text-black">
-            Unlimited movies
             <strong className="block font-extrabold text-[#1100b3]">
-              anytime, anywhere.
+              {random.original_title}
             </strong>
           </h1>
 
           <p className="mt-4 max-w-lg sm:text-xl/relaxed text-center mx-auto text-black">
-            Kinema has a large collection of Movies and Web Series available in
-            various qualities with subtitles.
+            {random.overview}
           </p>
 
           <div className="mt-8 flex gap-4 text-center justify-center gap:2 items-center py-auto flex-wrap">
-            <input
-              type="search"
-              className="block w-full rounded bg-[#1100b3] px-12 py-3 mt-3 lg:my-0 text-sm font-medium text-white shadow focus:outline-none focus:ring sm:w-auto placeholder:text-slate-100"
-              placeholder="Enter a movie/series title"
-            />
+            <button className="block w-full rounded bg-[#1100b3] px-12 py-3 mt-3 lg:my-0 text-sm font-medium text-white shadow focus:outline-none focus:ring sm:w-auto placeholder:text-slate-100">
+              Watch Now
+            </button>
 
             <input
-              type="submit"
-              value="Search"
+              type="button"
+              value="Read More"
               className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-[#1100b3] shadow-sm hover:shadow-black  focus:outline-none focus:ring sm:w-auto"
             />
           </div>
--- kinema/src/utils/request.js
+++ kinema/src/utils/request.js
@@ -24,4 +24,22 @@ export const getTrendingMovies = async () => {
     const data = await res.json();
     return data.results;
   }
+
+  export const getPopularMovies = async() => {
+    // const res = await fetch(`${BASE_URL}/movie/popular?api_key={$API_KEY}`)
+    // const data = await res.json();
+    // return data.results;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjFiYWZiMWIzMjM1MTc3ZWQ0OTA0Mjc4YzE1NTI0OSIsInN1YiI6IjY1ODJkYmM4OTkyZmU2M2U3ZTcyNDIzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n-g9tn45Un-Sdi5beHrcrY5mGnPDJ_XVWzGLOtjwNtU'
+      }
+    };
+    
+    const res = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+    const data = await res.json();
+    return data.results;
+
+  }
   
