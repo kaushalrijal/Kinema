e08028256f7943ea0497d4dfa35ee24a47c20672
kaushalrijal <kaushalrijal091@gmail.com>
2024-01-13 18:16:01 +0545

    some more few minor tweaks

--- kinema/src/app/layout.tsx
+++ kinema/src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
